a9fcd3c66e833ca98904db7c61d4e20e7f478f73
DeinyACR <deinyacr@gmail.com>
2025-11-14 22:56:12 -0600

    creación de función

--- app.py
+++ app.py
@@ -1,9 +1,32 @@
 import streamlit as st
+import scipy.stats
+import time
 
 st.header('Lanzar una moneda')
+
+chart = st.line_chart([0.5])
+
+def toss_coin(n):
+    trial_outcomes = scipy.stats.bernoulli.rvs(p = 0.5, size = n)
+
+    mean = None
+    outcome_no = 0
+    outcome_1_count = 0
+
+    for r in trial_outcomes:
+        outcome_no += 1
+        if r== 1:
+            outcome_1_count += 1
+        mean = outcome_1_count / outcome_no
+        chart.add_rows([mean])
+        time.sleep(0.05)
+    return mean 
+
 number_of_trails = st.slider('¿Número de intentos?', 1, 100, 10)
 start_button = st.button('Ejecutar')
 
-if start_button: st.write(f'Experimento con {number_of_trails} intentos en curso.')
+if start_button:
+    st.write(f'Experimento con {number_of_trails} intentos en curso.')
+    mean = toss_coin(number_of_trails)
 
 st.write('Esta aplicación aún no es ficional. En construcción')
