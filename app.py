41ca0c6e65af3a9fa7d543c6031f2cd883419924
DeinyACR <deinyacr@gmail.com>
2025-11-14 22:24:15 -0600

    se agraga el slider y start button

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 import streamlit as st
 
 st.header('Lanzar una moneda')
+number_of_trails = st.slader('¿Número de intesntos?', 1, 100, 10)
+start_button = st.button('Ejecutar')
+
+if start_button: st.write(f'Experimento con {number_of_trails} intentos en curso.')
 
 st.write('Esta aplicación aún no es ficional. En construcción')
